73c1462cba9014dc470403ee75e9d86b0d02d6f9
Brian Kwong <briankwongg@gmail.com>
2014-10-15 22:03:07 -0400

    Simplification of Expr
    Successfully simplifies code as well as retrieves correct order in any sequence.

--- basicalgorithm/multi_expr.py
+++ basicalgorithm/multi_expr.py
@@ -1,31 +1,72 @@
 def algebra(expr):
     #splitting input string into list
     list_expr = expr.split()
-    num_list = []
     #finding positions of operators
-    for i, item in enumerate(list_expr):
-        if (item == '*' or item == '/'):
-            num_list.extend(str(i))
-    for i, item in enumerate(list_expr):
-        if (item == '+' or item == '-'):
-            num_list.extend(str(i))
-    print(num_list) #bugtest: print out indexes of operators
-    #defining numbers to operate
-    a = int(list_expr[int(num_list[0]) - 1])
-    b = int(list_expr[int(num_list[0]) + 1])
-    c = -b
-    #first operation
-    if ((list_expr[int(num_list[0])] == '*') | (list_expr[int(num_list[0])] == '/')):
-        if (list_expr[int(num_list[0])] == '*'):
-            x = a * b
-        if (list_expr[int(num_list[0])] == '/'):
-            x = a / b
-    if ((list_expr[int(num_list[0])] == '+') | (list_expr[int(num_list[0])] == '-')):
-        if (list_expr[int(num_list[0])] == '+'):
-            x = a + b
-        if (list_expr[int(num_list[0])] == '-'):
-            x = a + c
-    else:
-        pass
-    print(x) #value of first operator
-    
+    for j in list_expr:
+        num_list = []
+        for i, item in enumerate(list_expr):
+            if (item == '*' or item == '/'):
+                num_list.extend(str(i))
+        for i, item in enumerate(list_expr):
+            if (item == '+' or item == '-'):
+                num_list.extend(str(i))
+        print(num_list) #bugtest: print out indexes of operators
+        try:
+            int(j)
+        except ValueError:
+            #defining numbers to operate
+            a = int(list_expr[int(num_list[0]) - 1])
+            b = int(list_expr[int(num_list[0]) + 1])
+            c = -b
+            #first operation
+            if ((list_expr[int(num_list[0])] == '*') | (list_expr[int(num_list[0])] == '/')):
+                if (list_expr[int(num_list[0])] == '*'):
+                    x = a * b
+                if (list_expr[int(num_list[0])] == '/'):
+                    x = a / b
+            if ((list_expr[int(num_list[0])] == '+') | (list_expr[int(num_list[0])] == '-')):
+                if (list_expr[int(num_list[0])] == '+'):
+                    x = a + b
+                if (list_expr[int(num_list[0])] == '-'):
+                    x = a + c
+            else:
+                pass
+            print(x) #bugtest: print out result of first operator
+            list_expr.pop(int(num_list[0]) - 1)
+            list_expr.pop(int(num_list[0]))
+            list_expr[int(num_list[0]) - 1] = str(x)
+            print(list_expr)
+    for j in list_expr:
+        num_list = []
+        for i, item in enumerate(list_expr):
+            if (item == '*' or item == '/'):
+                num_list.extend(str(i))
+        for i, item in enumerate(list_expr):
+            if (item == '+' or item == '-'):
+                num_list.extend(str(i))
+        print(num_list) #bugtest: print out indexes of operators
+        try:
+            int(j)
+        except ValueError:
+            #defining numbers to operate
+            a = int(list_expr[int(num_list[0]) - 1])
+            b = int(list_expr[int(num_list[0]) + 1])
+            c = -b
+            #first operation
+            if ((list_expr[int(num_list[0])] == '*') | (list_expr[int(num_list[0])] == '/')):
+                if (list_expr[int(num_list[0])] == '*'):
+                    x = a * b
+                if (list_expr[int(num_list[0])] == '/'):
+                    x = a / b
+            if ((list_expr[int(num_list[0])] == '+') | (list_expr[int(num_list[0])] == '-')):
+                if (list_expr[int(num_list[0])] == '+'):
+                    x = a + b
+                if (list_expr[int(num_list[0])] == '-'):
+                    x = a + c
+            else:
+                pass
+            print(x) #bugtest: print out result of first operator
+            list_expr.pop(int(num_list[0]) - 1)
+            list_expr.pop(int(num_list[0]))
+            list_expr[int(num_list[0]) - 1] = str(x)
+            print(list_expr)          
