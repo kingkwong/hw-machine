a851b3dfb04aa2126ffdbf1e02000af810127fae
Brian Kwong <briankwongg@gmail.com>
2014-10-18 19:09:14 -0400

    Added Decimal Support
    Changed values from Int() to Float(); Added String to Int() or Float() converter to get most simplified result; More bugfixes

--- basicalgorithm/multi_expr.py
+++ basicalgorithm/multi_expr.py
@@ -1,3 +1,4 @@
+from ast import literal_eval
 def algebra(expr):
     #splitting input string into list
     list_expr = expr.split()
@@ -15,14 +16,14 @@ def algebra(expr):
                 num_list.extend(str(i))
         #print(num_list) #bugtest: print out indexes of operators
         try:
-            int(j)
+            float(j)
         except ValueError:
             #defining numbers to operate
-            a = int(list_expr[int(num_list[0]) - 1])
-            b = int(list_expr[int(num_list[0]) + 1])
+            a = float(list_expr[int(num_list[0]) - 1])
+            b = float(list_expr[int(num_list[0]) + 1])
             c = -b
             #perform first operation 
-            if (list_expr[int(num_list[0])] == '^')
+            if (list_expr[int(num_list[0])] == '^'):
                 x = a ** b
             if ((list_expr[int(num_list[0])] == '*') | (list_expr[int(num_list[0])] == '/')):
                 if (list_expr[int(num_list[0])] == '*'):
@@ -40,7 +41,7 @@ def algebra(expr):
             list_expr.pop(int(num_list[0]) - 1)
             list_expr.pop(int(num_list[0]))
             list_expr[int(num_list[0]) - 1] = str(x)
-            print(list_expr)
+            #print(list_expr)
     for j in list_expr:
         num_list = []
         for i, item in enumerate(list_expr):
@@ -54,14 +55,14 @@ def algebra(expr):
                 num_list.extend(str(i))
         #print(num_list) #bugtest: print out indexes of operators
         try:
-            int(j)
+            float(j)
         except ValueError:
             #defining numbers to operate
-            a = int(list_expr[int(num_list[0]) - 1])
-            b = int(list_expr[int(num_list[0]) + 1])
+            a = float(list_expr[int(num_list[0]) - 1])
+            b = float(list_expr[int(num_list[0]) + 1])
             c = -b
             #perform additional operations
-            if (list_expr[int(num_list[0])] == '^')
+            if (list_expr[int(num_list[0])] == '^'):
                 x = a ** b
             if ((list_expr[int(num_list[0])] == '*') | (list_expr[int(num_list[0])] == '/')):
                 if (list_expr[int(num_list[0])] == '*'):
@@ -76,8 +77,12 @@ def algebra(expr):
             else:
                 pass
             #print(x) #prints result of each operation
-        print(x) #print answer
             list_expr.pop(int(num_list[0]) - 1)
             list_expr.pop(int(num_list[0]))
             list_expr[int(num_list[0]) - 1] = str(x)
-            #print(list_expr) #prints simplification of expression each time         
+            #print(list_expr) #prints simplification of expression each time
+    val = literal_eval(str(x))
+    if (isinstance(val, int) or (isinstance(val, float) and val.is_integer()) == True):
+        print(int(x))
+    else:
+        print(x)
